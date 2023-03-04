8f31d4bdf3287e0588154cc40f00f2a15733b424
wocjechdominiak <wojciech.dominiak96@gmail.com>
2023-03-04 17:40:58 +0100

    added usestate to formDialog

--- components/ui/FormDialog/FormDialog.tsx
+++ components/ui/FormDialog/FormDialog.tsx
@@ -17,6 +17,10 @@ export default function FormDialog({ label }: IProps) {
   const [open, setOpen] = React.useState(false);
   const { dispatch } = React.useContext(BalanceContext);
 
+  const [name, setName] = React.useState<string>("");
+  const [amount, setAmount] = React.useState<number>(0);
+  const [date, setDate] = React.useState<string>("");
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -29,10 +33,10 @@ export default function FormDialog({ label }: IProps) {
     dispatch({
       type: balanceActions.add,
       element: {
-        name: "test",
-        id: "1",
-        date: "03-01-2202",
-        amount: 10.42,
+        name: name,
+        id: Math.random().toString(),
+        date: date,
+        amount: amount,
       },
     });
     setOpen(false);
@@ -48,6 +52,7 @@ export default function FormDialog({ label }: IProps) {
         <DialogContent>
           <DialogContentText>{label}</DialogContentText>
           <TextField
+            onChange={(e) => setName(e.target.value)}
             autoFocus
             margin="dense"
             id="name"
@@ -57,6 +62,7 @@ export default function FormDialog({ label }: IProps) {
             variant="standard"
           />
           <TextField
+            onChange={(e) => setAmount(+e.target.value)}
             autoFocus
             margin="dense"
             id="amount"
@@ -66,6 +72,7 @@ export default function FormDialog({ label }: IProps) {
             variant="standard"
           />
           <TextField
+            onChange={(e) => setDate(e.target.value)}
             id="date"
             label="Data"
             type="date"
